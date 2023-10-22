6494ec3dfb6394cb8d197374f06a0d2af2ca283f
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-22 22:23:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ vertexShader: `
   attribute float size;
   void main() {
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-    gl_PointSize = 1.0
+    gl_PointSize = 1.0;
     gl_Position = projectionMatrix * mvPosition;
   }
 `,
