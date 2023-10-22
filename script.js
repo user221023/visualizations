16cd454a57a9cbc9944728087f3970d9bed3e55b
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-22 22:35:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,130 +2,76 @@ import * as THREE from './three.module.js';
 import { OrbitControls } from './OrbitControls.js';
 
 async function fetchData() {
-    let points;
-    try {
-        const response = await fetch('https://gist.githubusercontent.com/user221023/644135557782c9f4bcbf26365644ce95/raw/fdc9fc054d27762653d366449d065718825d731f/data.json');
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        points = await response.json();
-    } catch (error) {
-        console.error('Error fetching data: ', error);
-        return;
+  try {
+    const response = await fetch('https://gist.githubusercontent.com/user221023/644135557782c9f4bcbf26365644ce95/raw/fdc9fc054d27762653d366449d065718825d731f/data.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
     }
+    const points = await response.json();
+    init(points);
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+  }
+}
 
-    // Initialize the scene, camera, and renderer
-    var scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000000);
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+function init(points) {
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
 
-    // Define Colors
-    var darkColor = 0x5099d1;
-    var mediumColor = 0x48dcf6;
-    var lightColor = 0x50ffb;
-    var lightingColor = 0xf3d2ef;
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(1, 1, 1);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    // Position the camera to see the lit side
-    camera.position.set(1, 1, 1);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    // Initialize OrbitControls
-    var controls = new OrbitControls(camera, renderer.domElement);
-    controls.update();
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
 
-    // Add Ambient Light
-    var ambientLight = new THREE.AmbientLight(lightingColor);
-    scene.add(ambientLight);
+  const ambientLight = new THREE.AmbientLight(0xf3d2ef);
+  scene.add(ambientLight);
 
-    // Add Directional Light
-    var directionalLight = new THREE.DirectionalLight(lightingColor, 1);
-    directionalLight.position.set(1, 1, 1).normalize();
-    scene.add(directionalLight);
+  const directionalLight = new THREE.DirectionalLight(0xf3d2ef, 1);
+  directionalLight.position.set(1, 1, 1).normalize();
+  scene.add(directionalLight);
 
-    // Convert Points to a flat array
-    var flatPointsArray = [];
-    points.forEach(p => {
-        flatPointsArray.push(p.X, p.Y, p.Z);
-    });
+  const flatPointsArray = [];
+  points.forEach(p => flatPointsArray.push(p.X, p.Y, p.Z));
 
-      // Define band material h
-  var bandMaterial = new THREE.MeshPhysicalMaterial({
-    color: mediumColor,
+  const bandMaterial = new THREE.MeshPhysicalMaterial({
+    color: 0x48dcf6,
     transparent: true,
     opacity: 0.8,
     side: THREE.DoubleSide,
-    emissive: lightColor,
+    emissive: 0x50ffb,
     emissiveIntensity: 0.2
   });
-    
-    // Create the spherical band
-    var bandRadius = 1;
-    var bandGeometry = new THREE.SphereGeometry(bandRadius, 32, 32, 0, Math.PI * 2, Math.PI / 3, Math.PI / 3);
 
-const pointsMaterial = new THREE.ShaderMaterial({
-  uniforms: {
-    color: { value: new THREE.Color(darkColor) },
-  },
-vertexShader: `
-attribute float size;
-void main() {
-  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
-  gl_PointSize = size * (300.0 / -mvPosition.z); // You can adjust the size multiplier here as needed
-  gl_Position = projectionMatrix * mvPosition;
-}
-`,
+  const bandGeometry = new THREE.SphereGeometry(1, 32, 32, 0, Math.PI * 2, Math.PI / 3, Math.PI / 3);
+  const bandMesh = new THREE.Mesh(bandGeometry, bandMaterial);
+  bandMesh.rotation.x = Math.PI / 2;
+  scene.add(bandMesh);
 
-fragmentShader: `
-uniform vec3 color;
-void main() {
-  vec2 coords = 2.0 * gl_PointCoord - 1.0; // Transform to [-1, 1] range
-  float dist = dot(coords, coords);
-  if (dist > 1.0) discard; // Discard fragments outside of unit circle
-  gl_FragColor = vec4(color, 1.0);
-}
+  const pointsMaterial = new THREE.PointsMaterial({
+    color: 0x5099d1,
+    size: 5,
+    transparent: true,
+    opacity: 0.8,
+    depthWrite: false
+  });
 
-`,
+  const pointsGeometry = new THREE.BufferGeometry();
+  pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(flatPointsArray, 3));
+  const pointsMesh = new THREE.Points(pointsGeometry, pointsMaterial);
+  scene.add(pointsMesh);
 
-depthTest: false,
-
-blending: THREE.NormalBlending,
-
-    
-  blending: THREE.AdditiveBlending,
-  depthTest: false,
-  transparent: true
-});
-
-
-    var bandMesh = new THREE.Mesh(bandGeometry, bandMaterial);
-    bandMesh.rotation.x = Math.PI / 2;
-    scene.add(bandMesh);
-
-// Convert Points to a flat array
-var flatPointsArray = [];
-var sizes = [];
-points.forEach(p => {
-    flatPointsArray.push(p.X, p.Y, p.Z);
-    sizes.push(5.0); // Adjust size as needed
-});
-
-var pointsGeometry = new THREE.BufferGeometry();
-pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(flatPointsArray, 3));
-pointsGeometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1));
-
-var pointsMesh = new THREE.Points(pointsGeometry, pointsMaterial);
-scene.add(pointsMesh);
-
-    // Render the scene
-    function animate() {
-        requestAnimationFrame(animate);
-        controls.update();
-        renderer.render(scene, camera);
-    }
-    animate();
+  function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  }
+  animate();
 }
 
 fetchData();
