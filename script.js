c5f17456fbea586dd16cd0e290e99a2933e2e439
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-22 15:59:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
-import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'https://cdn.skypack.dev/three@0.149.0';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.149.0/examples/js/controls/OrbitControls';
 
 async function fetchData() {
   let points;
