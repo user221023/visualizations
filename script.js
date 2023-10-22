0a905887ca42e765848d7183dbd3e8743cc20d3b
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-22 16:02:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.149.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.149.0/examples/jsm/controls/OrbitControls.js';
-
 async function fetchData() {
   let points;
   try {
