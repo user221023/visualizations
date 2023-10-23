6bff6e624f0e041898b06fadd3af7099d12e7adb
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 09:08:34 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,56 +1,58 @@
-import * as THREE from './three.module.js';
-import { OrbitControls } from './OrbitControls.js';
+    import * as THREE from './three.module.js';
+    import { OrbitControls } from './OrbitControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.sortObjects = true; // Ensure correct rendering order for transparent objects
-document.body.appendChild(renderer.domElement);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.sortObjects = true; // Ensure correct rendering order for transparent objects
+    document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(5, 32, 32);
-const material = new THREE.MeshStandardMaterial({
-  color: 0x48dcf6,
-  transparent: true,
-  opacity: 0.2,
-  wireframe: false,
-  side: THREE.DoubleSide // Render both sides of the mesh
-});
+    scene.background = new THREE.Color(0x000000); // Set background to black for better contrast
 
-const oblateSpheroid = new THREE.Mesh(geometry, material);
-scene.add(oblateSpheroid);
+    const geometry = new THREE.SphereGeometry(5, 32, 32);
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x48dcf6,
+      transparent: true,
+      opacity: 0.8,
+      wireframe: false,
+      side: THREE.DoubleSide // Render both sides of the mesh
+    });
 
-// Set up lighting
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(10, 10, 10);
-scene.add(light);
+    const oblateSpheroid = new THREE.Mesh(geometry, material);
+    scene.add(oblateSpheroid);
 
-// Set up ambient light
-const ambientLight = new THREE.AmbientLight(0x404040); // Soft white light
-scene.add(ambientLight);
+    // Set up lighting
+    const light = new THREE.PointLight(0xffffff, 5, 100); // Increased intensity
+    light.position.set(0, 0, 10);
+    scene.add(light);
 
-camera.position.z = 15;
+    // Set up ambient light
+    const ambientLight = new THREE.AmbientLight(0x404040); // Soft white light
+    scene.add(ambientLight);
 
-// Manipulate vertices to create oblate spheroid shape
-const positions = geometry.attributes.position;
-positions.needsUpdate = true;
-for (let i = 0; i < positions.count; i++) {
-  const y = positions.getY(i);
-  const scaleFactor = 1 - (0.4 * (Math.abs(y) / 5) ** 2); // Adjust flattening
-  positions.setY(i, y * scaleFactor);
-}
+    camera.position.z = 15;
 
-geometry.computeVertexNormals();
+    // Manipulate vertices to create oblate spheroid shape
+    const positions = geometry.attributes.position;
+    positions.needsUpdate = true;
+    for (let i = 0; i < positions.count; i++) {
+      const y = positions.getY(i);
+      const scaleFactor = 1 - (0.4 * (Math.abs(y) / 5) ** 2); // Adjust flattening
+      positions.setY(i, y * scaleFactor);
+    }
 
-// Set up OrbitControls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
+    geometry.computeVertexNormals();
 
-const animate = function () {
-  requestAnimationFrame(animate);
-  controls.update(); // Only required if controls.enableDamping = true, or if controls.autoRotate = true
-  renderer.render(scene, camera);
-};
+    // Set up OrbitControls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
 
-animate();
+    const animate = function () {
+      requestAnimationFrame(animate);
+      controls.update(); // Only required if controls.enableDamping = true, or if controls.autoRotate = true
+      renderer.render(scene, camera);
+    };
 
+    animate();
+  </script>
