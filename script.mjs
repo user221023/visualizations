38eab1e46d41c1e61ed6f896416e2f2d05616481
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 13:17:16 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -12,13 +12,14 @@ scene.background = new THREE.Color(0x000000); // Set background to black for bet
 const geometry = new THREE.SphereGeometry(5, 128, 128);
 
 const vertexShader = `
-precision highp float;
+varying vec3 vOriginalPosition;
 varying vec3 vPosition;
-varying vec3 vNormal;
 void main() {
-  vPosition = position;
-  vNormal = normalize(normalMatrix * normal);
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  vOriginalPosition = position;
+  vec3 flattenedPosition = position;
+  flattenedPosition.y *= 1.0 - 0.2 * pow(abs(flattenedPosition.y) / 5.0, 2.0); // Adjust flattening
+  vPosition = flattenedPosition;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(flattenedPosition, 1.0);
 }
 `;
 
@@ -37,13 +38,18 @@ float sigmoid(float x) {
 }
 
 void main() {
-    float distanceFromCenter = length(vPosition);
-    float normalizedDistance = distanceFromCenter / 5.0; // assuming the sphere radius is 5
-    float shellThickness = 0.1; // you can adjust this value
-    float fadeFactor = sigmoid(normalizedDistance);
+    float originalDistance = length(vOriginalPosition) / 5.0;
+    float fadeFactor = sigmoid(originalDistance);
     fadeFactor = 0.05 + 0.95 * fadeFactor; // Ensures fadeFactor doesn't go below 0.05
-
-    vec3 chosenColor = mix(interiorColor, exteriorColor, smoothstep(1.0 - shellThickness, 1.0, normalizedDistance));
+    
+    // Calculate the view direction based on the flattened position
+    vec3 viewDirection = normalize(vPosition);
+    float dotProduct = dot(viewDirection, vNormal);
+    
+    // Determine the color based on the original position
+    float originalNormalizedDistance = length(vOriginalPosition) / 5.0;
+    vec3 chosenColor = mix(interiorColor, exteriorColor, smoothstep(1.0 - shellThickness, 1.0, originalNormalizedDistance));
+    
     gl_FragColor = vec4(chosenColor, opacity * fadeFactor);
 }
 `;
