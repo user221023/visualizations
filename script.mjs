df8e3f2dc969f0ac4ce053de616a39e60d958987
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 08:26:46 +0200

    Update and rename script.js to script.mjs

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
