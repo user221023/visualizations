901f7e06a0618f4fa1fe2887d7f73b91480ee15f
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 13:11:30 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -5,7 +5,6 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.sortObjects = true; // Ensure correct rendering order for transparent objects
 document.body.appendChild(renderer.domElement);
 
 scene.background = new THREE.Color(0x000000); // Set background to black for better contrast
@@ -13,16 +12,18 @@ scene.background = new THREE.Color(0x000000); // Set background to black for bet
 const geometry = new THREE.SphereGeometry(5, 128, 128);
 
 const vertexShader = `
+precision highp float;
 varying vec3 vPosition;
-varying vec3 vNormal;  // Add this line to declare the varying for the normal
+varying vec3 vNormal;
 void main() {
   vPosition = position;
-  vNormal = normal; // Add this line to pass the normal to the fragment shader
+  vNormal = normalize(normalMatrix * normal);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 `;
 
 const fragmentShader = `
+precision highp float;
 uniform float slider1;
 uniform float slider2;
 uniform vec3 exteriorColor;
@@ -50,8 +51,7 @@ void main() {
 
 const settings = {
     slider1: 0.1,
-    slider2: 0.8,
-    slider3: 0.5
+    slider2: 0.8
 };
 
 // Initialize dat.GUI
@@ -63,17 +63,13 @@ gui.add(settings, 'slider1', 0.0, 0.3).onChange(value => {
 gui.add(settings, 'slider2', 0.5, 1).onChange(value => {
     uniforms.slider2.value = value;
 });
-gui.add(settings, 'slider3', 0, 1).onChange(value => {
-    uniforms.slider3.value = value;
-});
 
 const uniforms = {
     exteriorColor: { value: new THREE.Color(0x48dcf6) }, // Replace with your exterior color
     interiorColor: { value: new THREE.Color(0x9a566f) }, // Replace with your interior color
     opacity: { value: 0.8 },
     slider1: { value: settings.slider1 },
-    slider2: { value: settings.slider2 },
-    slider3: { value: settings.slider3 }
+    slider2: { value: settings.slider2 }
 };
 
 const material = new THREE.ShaderMaterial({
@@ -82,37 +78,35 @@ const material = new THREE.ShaderMaterial({
     uniforms,
     transparent: true,
     side: THREE.DoubleSide,
-    depthWrite: false,
+    depthWrite: false // Keeping this as false since object is transparent
 });
 
 const oblateSpheroid = new THREE.Mesh(geometry, material);
 scene.add(oblateSpheroid);
 
-    // Set up lighting
-    const light = new THREE.PointLight(0xffffff, 5, 100); // Increased intensity
-    light.position.set(0, 0, 10);
-    scene.add(light);
+// Set up lighting
+const light = new THREE.PointLight(0xffffff, 5, 100);
+light.position.set(0, 0, 10);
+scene.add(light);
 
-    // Set up ambient light
-    const ambientLight = new THREE.AmbientLight(0x404040); // Soft white light
-    scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
 
-    camera.position.z = 15;
+camera.position.z = 15;
 
-    // Manipulate vertices to create oblate spheroid shape
-    const positions = geometry.attributes.position;
-    positions.needsUpdate = true;
-    for (let i = 0; i < positions.count; i++) {
-      const y = positions.getY(i);
-      const scaleFactor = 1 - (0.2 * (Math.abs(y) / 5) ** 2); // Adjust flattening
-      positions.setY(i, y * scaleFactor);
-    }
+// Manipulate vertices to create oblate spheroid shape
+const positions = geometry.attributes.position;
+positions.needsUpdate = true;
+for (let i = 0; i < positions.count; i++) {
+  const y = positions.getY(i);
+  const scaleFactor = 1 - (0.2 * (Math.abs(y) / 5) ** 2);
+  positions.setY(i, y * scaleFactor);
+}
 
-    geometry.computeVertexNormals();
+geometry.computeVertexNormals();
 
-    // Set up OrbitControls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.update();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
 const animate = function () {
     requestAnimationFrame(animate);
