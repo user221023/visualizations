fc763a45fb8757e6eebc6e37e52b83b88cd365da
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 12:23:33 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,40 +1,16 @@
-    import * as THREE from './three.module.js';
-    import { OrbitControls } from './OrbitControls.js';
+import * as THREE from './three.module.js';
+import { OrbitControls } from './OrbitControls.js';
 
-// Create an object to hold the values
-const settings = {
-    slider1: 0.9,
-    slider2: 25.0,
-    slider3: 0.5
-};
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.sortObjects = true; // Ensure correct rendering order for transparent objects
+document.body.appendChild(renderer.domElement);
 
-// Initialize dat.GUI
-const gui = new dat.GUI();
+scene.background = new THREE.Color(0x000000); // Set background to black for better contrast
 
-// Add the sliders
-gui.add(settings, 'slider1', 0, 1).onChange(value => {
-    // Update the uniform or any other variable when the slider changes
-    uniforms.someUniform.value = value;
-});
-gui.add(settings, 'slider2', 0, 50).onChange(value => {
-    // Update the uniform or any other variable when the slider changes
-    uniforms.someUniform.value = value;
-});
-gui.add(settings, 'slider3', 0, 1).onChange(value => {
-    // Update the uniform or any other variable when the slider changes
-    uniforms.someUniform.value = value;
-});
-
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.sortObjects = true; // Ensure correct rendering order for transparent objects
-    document.body.appendChild(renderer.domElement);
-
-    scene.background = new THREE.Color(0x000000); // Set background to black for better contrast
-
-    const geometry = new THREE.SphereGeometry(5, 32, 32);
+const geometry = new THREE.SphereGeometry(5, 32, 32);
 
 const vertexShader = `
 varying vec3 vPosition;
@@ -60,29 +36,46 @@ void main() {
     float fadeFactor = sigmoid(distanceFromCenter);
     gl_FragColor = vec4(color, opacity * fadeFactor);
 }
-
 `;
 
-const uniforms = {
-  color: { value: new THREE.Color(0x48dcf6) },
-  opacity: { value: 0.5 },
-  slider1: { value: guiControls.slider1 }, 
-  slider2: { value: guiControls.slider2 },
-  slider3: { value: guiControls.slider3 },
+const settings = {
+    slider1: 0.9,
+    slider2: 25.0,
+    slider3: 0.5
 };
 
+// Initialize dat.GUI
+const gui = new dat.GUI();
 
-    const material = new THREE.ShaderMaterial({
-      vertexShader,
-      fragmentShader,
-      uniforms,
-      transparent: true,
-      side: THREE.DoubleSide,
-      depthWrite: false,
-    });
+gui.add(settings, 'slider1', 0, 1).onChange(value => {
+    uniforms.slider1.value = value;
+});
+gui.add(settings, 'slider2', 0, 50).onChange(value => {
+    uniforms.slider2.value = value;
+});
+gui.add(settings, 'slider3', 0, 1).onChange(value => {
+    uniforms.slider3.value = value;
+});
 
-    const oblateSpheroid = new THREE.Mesh(geometry, material);
-    scene.add(oblateSpheroid);
+const uniforms = {
+    color: { value: new THREE.Color(0x48dcf6) },
+    opacity: { value: 0.5 },
+    slider1: { value: settings.slider1 },
+    slider2: { value: settings.slider2 },
+    slider3: { value: settings.slider3 }
+};
+
+const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms,
+    transparent: true,
+    side: THREE.DoubleSide,
+    depthWrite: false,
+});
+
+const oblateSpheroid = new THREE.Mesh(geometry, material);
+scene.add(oblateSpheroid);
 
     // Set up lighting
     const light = new THREE.PointLight(0xffffff, 5, 100); // Increased intensity
@@ -110,12 +103,10 @@ const uniforms = {
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.update();
 
-    const animate = function () {
-        uniforms.slider1.value = guiControls.slider1;
-       uniforms.slider2.value = guiControls.slider2;
-        requestAnimationFrame(animate);
-      controls.update(); // Only required if controls.enableDamping = true, or if controls.autoRotate = true
-      renderer.render(scene, camera);
-    };
+const animate = function () {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+};
 
-    animate();
+animate();
