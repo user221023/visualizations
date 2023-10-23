784cdc0d2a53ce912c0420d7cfb1bf5732f040ba
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 08:59:15 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -3,36 +3,54 @@ import { OrbitControls } from './OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.sortObjects = true; // Ensure correct rendering order for transparent objects
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.SphereGeometry(5, 32, 32);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+const material = new THREE.MeshStandardMaterial({
+  color: 0x00ff00,
+  transparent: true,
+  opacity: 0.5,
+  wireframe: false,
+  side: THREE.DoubleSide // Render both sides of the mesh
+});
+
 const oblateSpheroid = new THREE.Mesh(geometry, material);
 scene.add(oblateSpheroid);
 
+// Set up lighting
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+// Set up ambient light
+const ambientLight = new THREE.AmbientLight(0x404040); // Soft white light
+scene.add(ambientLight);
+
 camera.position.z = 15;
 
-// Manipulate vertices
+// Manipulate vertices to create oblate spheroid shape
 const positions = geometry.attributes.position;
 positions.needsUpdate = true;
 for (let i = 0; i < positions.count; i++) {
   const y = positions.getY(i);
-const scaleFactor = 1 - (0.4 * (Math.abs(y) / 5) ** 2);
-positions.setY(i, y * scaleFactor);
+  const scaleFactor = 1 - (0.4 * (Math.abs(y) / 5) ** 2); // Adjust flattening
+  positions.setY(i, y * scaleFactor);
 }
 
 geometry.computeVertexNormals();
 
-// Create OrbitControls
+// Set up OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+controls.update();
 
 const animate = function () {
   requestAnimationFrame(animate);
-  controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+  controls.update(); // Only required if controls.enableDamping = true, or if controls.autoRotate = true
   renderer.render(scene, camera);
 };
 
 animate();
+
