d3db073337e1a919ecb24618f3f2d608d51fcf5c
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 08:52:03 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/js/controls/OrbitControls.js';
+import { OrbitControls } from './OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
