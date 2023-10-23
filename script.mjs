2250a83696fbfbac236b0e738dff9c460240b1c0
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 08:37:28 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -6,14 +6,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
 const geometry = new THREE.SphereGeometry(5, 32, 32); // radius, widthSegments, heightSegments
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const oblateSpheroid = new THREE.Mesh(geometry, material);
