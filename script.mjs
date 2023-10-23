658ce3b3f2118cdef339883d5b253050c7c4f7c2
user221023 <148694388+user221023@users.noreply.github.com>
2023-10-23 13:07:45 +0200

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -14,8 +14,10 @@ const geometry = new THREE.SphereGeometry(5, 128, 128);
 
 const vertexShader = `
 varying vec3 vPosition;
+varying vec3 vNormal;  // Add this line to declare the varying for the normal
 void main() {
   vPosition = position;
+  vNormal = normal; // Add this line to pass the normal to the fragment shader
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 `;
